b258db6e2c10c006e2006ca1de7e132bf371c676
ibrahimyebdri <ib.yebdri@gmail.com>
2026-06-01 21:09:40 +0100

    Update UI, fix web and mobile issues, improve series and episode pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,25 @@ import { colors } from "@/theme/colors";
 import { FavoritesProvider } from "@/context/FavoritesContext";
 import { HistoryProvider } from "@/context/HistoryContext";
 
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  useFonts({
+  const [fontsLoaded] = useFonts({
     ...Ionicons.font,
   });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
